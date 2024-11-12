782ad84ed7cb2a8b9ee2c64d9049d6b298c2c380
rrrr <vegaruvit@gmail.com>
2024-11-12 16:07:31 +0900

    create item selector

---
+++ Converter/GenderSelector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Wpf_Basics.Models;
+
+namespace Wpf_Basics.Converter
+{
+    internal class GenderSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            Student? student = item as Student;
+            FrameworkElement? frameworkElement = container as FrameworkElement;
+
+            return frameworkElement?.FindResource(student?.Gender == "남" ? "Man" : "Women") as DataTemplate;
+        }
+    }
+}
