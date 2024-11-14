455a269403f618b5c9e2029017b0691773425667
rrrr <vegaruvit@gmail.com>
2024-11-14 22:34:11 +0900

    create comboitems

--- ViewModels/ControlTemplateViewModel.cs
+++ ViewModels/ControlTemplateViewModel.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Wpf_Basics.Models;
 
 namespace Wpf_Basics.ViewModels
 {
     class ControlTemplateViewModel : Bases.ViewModelBase
     {
+        public List<ComboItem> Items { get; set; }
+        public ControlTemplateViewModel()
+        {
+            this.Items = ComboItem.Items;
+        }
     }
 }
