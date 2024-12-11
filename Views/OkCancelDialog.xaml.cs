f1f56bf5f5e15be6be0c7afd334c522eaff84149
rrrr <vegaruvit@gmail.com>
2024-12-11 15:15:35 +0900

    build DialogWindow

--- Views/OkCancelDialog.xaml.cs
+++ Views/OkCancelDialog.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Wpf_Basics
+namespace Wpf_Basics.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// OkCancelDialog.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class OkCancelDialog : Window
     {
-        public MainWindow()
+        public OkCancelDialog()
         {
             InitializeComponent();
-        }
-
-        private void ShowDialog(object sender, RoutedEventArgs e)
-        {
-            
         }
     }
 }
